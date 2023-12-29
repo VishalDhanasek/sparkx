e4250477a1d8c65489b056a7f72ca8b0500bcf67
VishalDhanasek <veeruvishal4@gmail.com>
2023-12-29 11:30:26 +0530

    update organizers

--- components/organizers.tsx
+++ components/organizers.tsx
@@ -17,12 +17,6 @@ export default function Organizers() {
 {/* Items */}
 <div className="max-w-sm md:max-w-4xl mx-auto grid gap-2 grid-cols-4 md:grid-cols-4">
 
-{/* Item */}
-<div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
-  <img className="max-w-full fill-current text-gray-400" src="/dreamtn.png" width="114" height="14">
-    
-  </img>
-</div>
 
 {/* Item */}
 <div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
@@ -33,6 +27,14 @@ export default function Organizers() {
 
 {/* Item */}
 <div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
+  <img className="max-w-full fill-current text-gray-400" src="/growbinar.png" width="194" height="24">
+    
+  </img>
+</div>
+
+
+{/* Item */}
+<div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
   <img className="max-w-full fill-current text-gray-400" src="/startuptn.png" width="194" height="24">
     
   </img>
@@ -40,7 +42,7 @@ export default function Organizers() {
 
 {/* Item */}
 <div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
-  <img className="max-w-full fill-current text-gray-400" src="/growbinar.png" width="194" height="24">
+  <img className="max-w-full fill-current text-gray-400" src="/dreamtn.png" width="114" height="14">
     
   </img>
 </div>
