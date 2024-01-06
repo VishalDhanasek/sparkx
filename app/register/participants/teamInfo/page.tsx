b30462bd410411927f9e3fec33f7adf32b2bf12a
Hari Krishna moorthy <harikrishna.moorthy@shopup.org>
2024-01-06 18:02:05 +0530

    Update register

--- app/register/participants/teamInfo/page.tsx
+++ app/register/participants/teamInfo/page.tsx
@@ -2,15 +2,15 @@ export const metadata = {
     title: 'Team Info',
     description: 'Page description',
   }
-  
+
   import Link from 'next/link'
-  
+
   export default function TeamInfo() {
     return (
       <section className="bg-gradient-to-b from-gray-100 to-white">
         <div className="max-w-9xl mx-auto px-4 sm:px-6">
           <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-  
+
             {/* Page header */}
             <div className="max-w-3xl mx-auto text-center pb-12 md:pb-5">
             <ol className="max-w-3xl mx-auto text-center pb-12 md:pb-20 items-center w-full space-y-4 sm:flex sm:space-x-[150px] sm:space-y-0 rtl:space-x-reverse">
@@ -47,8 +47,8 @@ export const metadata = {
 
 
 
-            
-  
+
+
             {/* Form */}
             <div className="max-w-2xl mx-auto">
             <h2 className="mb-4 text-xl font-bold text-gray-800 ">Team Details</h2>
@@ -140,7 +140,7 @@ export const metadata = {
                     <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="email">Phone Number <span className="text-red-600">*</span></label>
                     <input id="text" type="phone-input" className="form-input w-full text-gray-800" placeholder="Enter your phone number" required />
                   </div>
-                </div>  
+                </div>
 
 
               <h2 className="mb-4 mt-10 text-xl font-bold text-gray-800 ">Team Member 4</h2>
@@ -159,31 +159,30 @@ export const metadata = {
                     <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="email">Phone Number <span className="text-red-600">*</span></label>
                     <input id="text" type="phone-input" className="form-input w-full text-gray-800" placeholder="Enter your phone number" required />
                   </div>
-                </div>      
-    
-             
+                </div>
+
+
                 <div className=" -mx-3 mb-20 mt-10 grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-5 float-right">
-              <Link href="/register" className="btn-sm text-white bg-blue-600 hover:bg-blue-700 ml-3 w-50 mb-20">
+              <Link href="/register/participants/" className="btn-sm text-white bg-blue-600 hover:bg-blue-700 ml-3 w-50 mb-20">
                   <span>Back</span>
                 </Link>
-                <Link href="/register/idea" className="btn-sm text-white bg-blue-600 hover:bg-blue-700 mb-20 ml-3">
+                <Link href="/register/participants/idea" className="btn-sm text-white bg-blue-600 hover:bg-blue-700 mb-20 ml-3">
                   <span>Next</span>
                   <svg className="w-3 h-3 fill-current text-white shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
                   </svg>
-                </Link>  
+                </Link>
               </div>
 
             </form>
-            
-            
-              
-              
+
+
+
+
             </div>
-  
+
           </div>
         </div>
       </section>
     )
   }
-  
