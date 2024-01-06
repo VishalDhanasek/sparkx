5e3e0376b752e5d3c07d68f853002946bc6689be
Hari Krishna moorthy <harikrishna.moorthy@shopup.org>
2024-01-06 17:58:11 +0530

    Update register

--- app/contactus/page.tsx
+++ app/contactus/page.tsx
@@ -1,29 +1,35 @@
 export const metadata = {
-    title: 'Contact Us',
-    description: 'Page description',
-  }
-  
-  import Link from 'next/link'
-  
-  export default function ContactUs() {
-    return (
-      <section className="bg-gradient-to-b from-gray-100 to-white">
-        <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        
-          <div className="flex flex-col sm:flex-row pt-32 pb-12 md:pt-40 md:pb-0" data-aos="zoom-y-out">
-            
-  
-            {/* Page header */}
-            <div className="lg:w-1/2 lg:mx-6   pb-12 md:pb-20">
-              <h1 className="h1 bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">Get in Touch!</h1>
-              <br/>
-              <h5 className="h4 text-gray-400">Drop your mail at</h5>
-              <h2 className="h4">info@innovationgeeks.in</h2>
-              <br/>
-              <br/>
+  title: "Contact Us",
+  description: "Page description",
+};
+
+import Link from "next/link";
+
+export default function ContactUs() {
+  return (
+    <section className="bg-gradient-to-b from-gray-100 to-white">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div
+          className="flex flex-col sm:flex-row pt-32 pb-12 md:pt-40 md:pb-0"
+          data-aos="zoom-y-out"
+        >
+          {/* Page header */}
+          <div className="lg:w-1/2 lg:mx-6   pb-12 md:pb-20">
+            <h1 className="h1 bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
+              Get in Touch!
+            </h1>
+            <br />
+            <h5 className="h4 text-gray-400">Drop your mail at</h5>
+            <h2 className="h4 mb-20">sparkx2024@gmail.com</h2>
 
 
-              <ul className="flex mb-4 md:order-1 md:ml-0 md:mb-0">
+            <h5 className="h4 mt-8 text-gray-400">Make a call to </h5>
+            <h2 className="h4">+91 80720 03967</h2>
+
+            <br />
+            <br />
+
+            {/* <ul className="flex mb-4 md:order-1 md:ml-0 md:mb-0">
               <li>
               <a href="https://www.linkedin.com/company/innovation-geeks-private-ltd" className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="LinkedIn" target="_blank">
                 <svg className="w-12 h-12 fill-current" viewBox="-8 -8 32 32" xmlns="http://www.w3.org/2000/svg">
@@ -37,55 +43,81 @@ export const metadata = {
                 </svg>
               </a>
             </li>
-            
+
             <li className="ml-4">
               <a href="https://www.instagram.com/innovation._.geeks/" className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Instagram" target="_blank">
                 <svg className="w-12 h-12 fill-current" viewBox="-8 -8 32 32" xmlns="http://www.w3.org/2000/svg">
                 <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/> </svg>
               </a>
             </li>
-          </ul>
+          </ul> */}
+          </div>
 
-
-              
-              
-            </div>
-
-            
-            
-  
-            {/* Form */}
-            <div className="lg:w-1/2 lg:mx-6 pb-12 md:pb-20">
-              <form>
-                <div className="flex flex-wrap -mx-3 mb-4">
-                  <div className="w-full px-3">
-                    <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="name">Name <span className="text-red-600">*</span></label>
-                    <input id="name" type="text"  className="form-input w-full text-gray-800" placeholder="Enter your name" required />
-                  </div>
+          {/* Form */}
+          <div className="lg:w-1/2 lg:mx-6 pb-12 md:pb-20">
+            <form>
+              <div className="flex flex-wrap -mx-3 mb-4">
+                <div className="w-full px-3">
+                  <label
+                    className="block text-gray-800 text-sm font-medium mb-1"
+                    htmlFor="name"
+                  >
+                    Name <span className="text-red-600">*</span>
+                  </label>
+                  <input
+                    id="name"
+                    type="text"
+                    className="form-input w-full text-gray-800"
+                    placeholder="Enter your name"
+                    required
+                  />
                 </div>
-                <div className="flex flex-wrap -mx-3 mb-4">
-                  <div className="w-full px-3">
-                    <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="email">Email <span className="text-red-600">*</span></label>
-                    <input id="email" type="email" className="form-input w-full text-gray-800" placeholder="Enter your email address" required />
-                  </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-4">
+                <div className="w-full px-3">
+                  <label
+                    className="block text-gray-800 text-sm font-medium mb-1"
+                    htmlFor="email"
+                  >
+                    Email <span className="text-red-600">*</span>
+                  </label>
+                  <input
+                    id="email"
+                    type="email"
+                    className="form-input w-full text-gray-800"
+                    placeholder="Enter your email address"
+                    required
+                  />
                 </div>
-                <div className="flex flex-wrap -mx-3 mb-4">
-                  <div className="w-full px-3">
-                    <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="long-text">Message <span className="text-red-600">*</span></label>
-                    <textarea id="long-text" name="long-text"  className="form-input w-full resize-none text-gray-800" placeholder="Type your Message" required />
-                  </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-4">
+                <div className="w-full px-3">
+                  <label
+                    className="block text-gray-800 text-sm font-medium mb-1"
+                    htmlFor="long-text"
+                  >
+                    Message <span className="text-red-600">*</span>
+                  </label>
+                  <textarea
+                    id="long-text"
+                    name="long-text"
+                    className="form-input w-full resize-none text-gray-800"
+                    placeholder="Type your Message"
+                    required
+                  />
                 </div>
-                <div className="flex flex-wrap -mx-3 mt-6">
-                  <div className="w-full px-3">
-                    <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full">Send</button>
-                  </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mt-6">
+                <div className="w-full px-3">
+                  <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full">
+                    Send
+                  </button>
                 </div>
-              </form>
-            </div>
-  
+              </div>
+            </form>
           </div>
         </div>
-      </section>
-    )
-  }
-  
+      </div>
+    </section>
+  );
+}
